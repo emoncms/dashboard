cce7dfb35bed90c835243d32fda0e47aa212645b
Paul Reed <pauldreed@gmail.com>
2016-11-30 19:38:53 +0000

    Bar widget enhancements (#65)
    Enable bar widget customization
    - Allow Title
    - Allow Label colour
    - Allow font selection
    - made the value slightly smaller to make it fit better into the bar when graduations are off.
    - should not break existing bar widgets. Tested locally but needs testing from others as well.

--- widget/bar/bar_render.js
+++ widget/bar/bar_render.js
@@ -30,7 +30,7 @@ function bar_widgetlist()
 	{
 		"bar":
 		{
-			"offsetx":-80,"offsety":-80,"width":160,"height":160,
+			"offsetx":-80,"offsety":-80,"width":160,"height":200,
 			"menu":"Widgets",
 			"options":    [],
 			"optionstype":[],
@@ -41,20 +41,36 @@ function bar_widgetlist()
 		}
 	};
 
-
-	var graduationDropBoxOptions = [
-					[1, "On"],
-					[0, "Off"]
+	var fontoptions = [
+					[9, "Arial Black"],
+					[8, "arial"],
+					[7, "Arial Narrow"],
+					[6, "sans-serif"],
+					[5, "Helvetica"],
+					[4, "Comic Sans MS"],
+					[3, "Courier New"],
+					[2, "arial"],
+					[1, "Georgia"],
+					[0, "Impact"]
 				];
+	
+	
+	var graduationDropBoxOptions = [
+					[0, "Off"],
+					[1, "On"]
+					];
 
-	addOption(widgets["bar"], "feedid",      "feedid",        _Tr("Feed"),            _Tr("Feed value"),                                                                  []);
-	addOption(widgets["bar"], "max",         "value",         _Tr("Max value"),       _Tr("Max value to show"),                                                           []);
-	addOption(widgets["bar"], "scale",       "value",         _Tr("Scale"),           _Tr("Value is multiplied by scale before display. Defaults to 1"),                  []);
-	addOption(widgets["bar"], "units",       "value",         _Tr("Units"),           _Tr("Unit type to show after value. Ex: <br>\"{Reading}{unit-string}\""),           []);
-	addOption(widgets["bar"], "offset",      "value",         _Tr("Offset"),          _Tr("Static offset. Subtracted from value before computing position (default 0)"),  []);
-	addOption(widgets["bar"], "colour",      "colour_picker", _Tr("Colour"),          _Tr("Colour to draw bar in"),                                                       []);
-	addOption(widgets["bar"], "graduations", "dropbox",       _Tr("Graduations"),     _Tr("Should the graduations be shown"),                                             graduationDropBoxOptions);
-	addOption(widgets["bar"], "gradNumber",  "value",         _Tr("Num Graduations"), _Tr("How many graduation lines to draw (only relevant if graduations are on)"),     []);
+	addOption(widgets["bar"], "title",			"value",			_Tr("Title"),			_Tr("Title of bar"),                                                                []);
+	addOption(widgets["bar"], "colour_label",	"colour_picker",	_Tr("Label Colour"),	_Tr("Colour of title and values"),			                                        []);
+	addOption(widgets["bar"], "font",     		"dropbox",       	_Tr("Font used"),		_Tr("Font used"),																	fontoptions);
+	addOption(widgets["bar"], "feedid",      	"feedid",        	_Tr("Feed"),			_Tr("Feed value"),                                                                  []);
+	addOption(widgets["bar"], "max",         	"value",         	_Tr("Max value"),       _Tr("Max value to show"),                                                           []);
+	addOption(widgets["bar"], "scale",       	"value",         	_Tr("Scale"),           _Tr("Value is multiplied by scale before display. Defaults to 1"),                  []);
+	addOption(widgets["bar"], "units",       	"value",         	_Tr("Units"),           _Tr("Unit type to show after value. Ex: <br>\"{Reading}{unit-string}\""),           []);
+	addOption(widgets["bar"], "offset",      	"value",         	_Tr("Offset"),          _Tr("Static offset. Subtracted from value before computing position (default 0)"),  []);
+	addOption(widgets["bar"], "colour",      	"colour_picker", 	_Tr("Colour"),          _Tr("Colour to draw bar in"),                                                       []);
+	addOption(widgets["bar"], "graduations", 	"dropbox",       	_Tr("Graduations"),     _Tr("Should the graduations be shown"),  											graduationDropBoxOptions);
+	addOption(widgets["bar"], "gradNumber",  	"value",         	_Tr("Num Graduations"), _Tr("How many graduation lines to draw (only relevant if graduations are on)"),     []);
 
 
 
@@ -70,6 +86,8 @@ function bar_draw()
 {
 	$('.bar').each(function(index)
 	{
+		var font = $(this).attr("font");
+		var title = $(this).attr("title");
 		var feedid = $(this).attr("feedid");
 		if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
 		var val = curve_value(feedid,dialrate);
@@ -79,16 +97,20 @@ function bar_draw()
 			var id = "can-"+$(this).attr("id");
 			var scale = 1*$(this).attr("scale") || 1;
 			draw_bar(widgetcanvas[id],
-								 0,
-								 0,
-								 $(this).width(),
-								 $(this).height(),val*scale,
-								 $(this).attr("max"),
-								 $(this).attr("units"),
-								 $(this).attr("colour"),
-								 $(this).attr("offset"),
-								 $(this).attr("graduations"),
-								 $(this).attr("gradNumber"));
+								0,
+								0,
+								$(this).attr("title"),
+								$(this).attr("font"),
+								$(this).width(),
+								$(this).height(),val*scale,
+								$(this).attr("max"),
+								$(this).attr("units"),
+								$(this).attr("colour"),
+								$(this).attr("colour_label"),
+								$(this).attr("offset"),
+								$(this).attr("graduations"),
+								$(this).attr("gradNumber")
+								);
 		}
 	});
 }
@@ -107,12 +129,15 @@ function bar_fastupdate()
 function draw_bar(context,
 				x_pos,				// these x and y coords seem unused?
 				y_pos,
+				title,
+				font,
 				width,
 				height,
 				raw_value,
 				max_value,
 				units_string,
 				display_colour,
+				colour_label,
 				static_offset,
 				graduationBool,
 				graduationQuant)
@@ -126,7 +151,8 @@ function draw_bar(context,
 	max_value = 1 * max_value || 3000;
 	// if units_string == false: "". Else units_string
 	units_string = units_string || "";
-
+	title = title || "";
+	colour_label = colour_label || "000";
 	static_offset = 1*static_offset || 0;
 	var display_value = raw_value
 	display_value = display_value-static_offset
@@ -140,9 +166,19 @@ function draw_bar(context,
 		size = width/2;
 	else
 		size = height/2;
-
-	size = size;
-
+	
+	if (font == 0){fontname = "Impact"}
+	if (font == 1){fontname = "Georgia"}
+	if (font == 2){fontname = "arial"}
+	if (font == 3){fontname = "Courier New"}
+	if (font == 4){fontname = "Comic Sans MS"}
+	if (font == 5){fontname = "Helvetica"}
+	if (font == 6){fontname = "sans-serif"}
+	if (font == 7){fontname = "Arial Narrow"}
+	if (font == 8){fontname = "Arial"}
+	if (font == 9){fontname = "Arial Black"}
+	else if (typeof(font) == "undefined") {fontname = "Arial Black"}
+	
 	if (graduationBool == 1)
 	{
 		height = height - (size/2)
@@ -163,9 +199,8 @@ function draw_bar(context,
 					border_space,
 					width-(border_space*2),
 					height-(border_space*2));
-
-
 	context.lineWidth = 0;
+	
 	if (display_colour.indexOf("#") == -1)			// Fix missing "#" on colour if needed
 		display_colour = "#" + display_colour;
 
@@ -182,40 +217,24 @@ function draw_bar(context,
 					width-(bar_border_space*2),
 					(height-bar_border_space) - bar_top );
 
+if (colour_label.indexOf("#") == -1)			// Fix missing "#" on colour if needed
+	colour_label = "#" + colour_label;
+					
 	if (graduationBool == 1)
 	{
-
-		// context.font = "bold "+(size*0.22)+"px arial";
-
-		// var posStrt = polar_to_cart(size/1.15, 90+spreadAngle, half_width, half_height);
-		// var posStop = polar_to_cart(size/1.15, 90-spreadAngle, half_width, half_height);
-
-		// context.save()
-		// context.translate(posStrt[0], posStrt[1]);
-		// context.rotate(deg_to_radians(-45));
-		// context.fillText(""+static_offset+units_string, 0, 0);        // Since we've translated the entire context, the coords we want to draw at are now at [0,0]
-		// context.restore();
-
-		// context.save(); // each context.save is only good for one restore, apparently.
-		// context.translate(posStop[0], posStop[1]);
-		// context.rotate(deg_to_radians(45));
-		// context.fillText(""+(static_offset+max_value)+units_string, 0, 0);
-		// context.restore();
-
-
-
 		if (graduationQuant > 0)
 		{
-
-			context.fillStyle = "#000";
+			context.fillStyle = colour_label;
 			context.textAlign    = "start";
-			context.font = "bold "+(size*0.25)+"px arial";
+			context.font = ((size*0.20)+"px "+ fontname);
 
 			var step = (height-border_space*2)/(Number(graduationQuant)+1);
 			var curY;
-
-			context.fillText((static_offset+max_value)+units_string, width+(size*0.1), 10+size*0.15);
+			
+			context.fillStyle = colour_label;
+			context.fillText((static_offset+max_value)+units_string, width+(size*0.1), (size*0.15)+2);
 			var divisions = Number(graduationQuant)+1;
+			
 			for (var y = 0; y < graduationQuant; y++)
 			{
 				curY = Number(((y+1)*step).toFixed(0))+0.5;  // Bin down so we're drawing in the middle of the pixel, so the line is exactly 1 px wide
@@ -227,24 +246,22 @@ function draw_bar(context,
 					unitOffset = unitOffset.toFixed(1)
 				else
 					unitOffset = unitOffset.toFixed(0)
-				context.fillText(unitOffset+units_string, width+(size*0.1), curY+(size*0.1));
+				context.fillStyle = colour_label;
+				context.fillText(unitOffset+units_string, width+(size*0.1), curY+(size*0.05));
 			}
-			context.fillText(static_offset+units_string, width+(size*0.1), height-10);
+			context.fillStyle = colour_label;
+			context.fillText(static_offset+units_string, width+(size*0.1), height-2);
 
 			context.strokeStyle = "#888";
 			context.stroke();
 		}
 	}
 
-
-	context.fillStyle = "#000";
-	context.textAlign    = "center";
-	context.font = "bold "+(size*0.55)+"px arial";
-	if (raw_value>100)
+	if (raw_value>=100)
 	{
 		raw_value = raw_value.toFixed(0);
 	}
-	else if (raw_value>10)
+	else if (raw_value>=10)
 	{
 		raw_value = raw_value.toFixed(1);
 	}
@@ -253,24 +270,52 @@ function draw_bar(context,
 		raw_value = raw_value.toFixed(2);
 	}
 
-
 	if (graduationBool == 1)
 	{
-		if (raw_value > 1000)		// Add additional offset to make alignment work for HUGE numbers
+		if (title === ""){
+			//if (raw_value > 1000)		// Add additional offset to make alignment work for HUGE numbers
 			half_width += (size*0.20)
-		context.fillText(raw_value+units_string, half_width+(size*0.25), height + (size*0.45));
+			//context.fillStyle = "#000";
+
+			context.textAlign    = "center";
+			context.fillStyle = colour_label;
+			context.font = ((size*0.3)+"px "+ fontname);
+			context.fillText(raw_value+units_string, half_width+(size*0.2), height + (size*0.3));
+			context.fillStyle = colour_label;
+			context.font = ((size*0.20)+"px "+ fontname);
+			context.textAlign    = "center";
+			context.fillText(title, half_width + (size * 0.35), height + (size * 0.2));					
+		}
+		else if (title) {
+		//if (raw_value > 1000)		// Add additional offset to make alignment work for HUGE numbers
+		half_width += (size*0.20)
+		//context.fillStyle = "#000";
+
+		context.textAlign    = "center";
+		context.fillStyle = colour_label;
+		context.font = ((size*0.25)+"px "+ fontname);
+		context.fillText(raw_value+units_string, half_width+(size*0.40), height + (size*0.48));
+		context.fillStyle = colour_label;
+		context.font = ((size*0.20)+"px "+ fontname);
+		context.textAlign    = "center";
+		context.fillText(title+":", half_width + (size * 0.35), height + (size * 0.2));
+		}
 	}
+	
 	else
 	{
+		//context.fillStyle = "#000";
+		context.fillStyle = colour_label;
+		context.textAlign    = "center";
+		context.font = ((size*0.5)+"px "+ fontname);
 		context.fillText(raw_value+units_string, half_width, height/2 + (size*0.2));
+		context.fillStyle = colour_label;
+		context.textAlign    = "center";
+		context.font = ((size*0.2)+"px "+ fontname);
+		context.fillText(title, half_width, height/7 + (size *0.1));
 	}
-
-
 
 	context.fillStyle = "#000";
 	var spreadAngle = 32;
 
-
 }
-
-
