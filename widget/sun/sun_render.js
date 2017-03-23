3e4ec8ab9c003a600a4821d919d9dd9371743cf3
vkylamba <vikas13jun@gmail.com>
2017-03-23 10:33:24 +0530

    Removed duplicate code

--- widget/sun/sun_render.js
+++ widget/sun/sun_render.js
@@ -39,13 +39,12 @@ function sun_widgetlist(){
 
   var fontoptions = [
     [9, "Arial Black"],
-    [8, "arial"],
     [7, "Arial Narrow"],
     [6, "sans-serif"],
     [5, "Helvetica"],
     [4, "Comic Sans MS"],
     [3, "Courier New"],
-    [2, "arial"],
+    [2, "Arial"],
     [1, "Georgia"],
     [0, "Impact"]
   ];
@@ -85,14 +84,13 @@ function sun_draw(){
 
       if (font == 0){fontname = "Impact"}
       if (font == 1){fontname = "Georgia"}
-      if (font == 2){fontname = "arial"}
+      if (font == 2){fontname = "Arial"}
       if (font == 3){fontname = "Courier New"}
       if (font == 4){fontname = "Comic Sans MS"}
       if (font == 5){fontname = "Helvetica"}
       if (font == 6){fontname = "sans-serif"}
       if (font == 7){fontname = "Arial Narrow"}
-      if (font == 8){fontname = "Arial"}
-      if (font == 9){fontname = "Arial Black"}
+      if (font == 8){fontname = "Arial Black"}
       else if (typeof(font) == "undefined") {fontname = "Arial Black"}
 
       if (color.indexOf("#") == -1) color = "#" + color;
