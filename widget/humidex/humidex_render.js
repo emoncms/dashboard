4889d099c73f09f1e6d3c0c5485245e8552cb225
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-06-11 01:07:38 +0200

    add license

--- widget/humidex/humidex_render.js
+++ widget/humidex/humidex_render.js
@@ -1,4 +1,11 @@
-
+  /*
+   All Emoncms code is released under the GNU Affero General Public License.
+   See COPYRIGHT.txt and LICENSE.txt.
+    ---------------------------------------------------------------------
+    Emoncms - open source energy visualisation
+    Part of the OpenEnergyMonitor project:
+    http://openenergymonitor.org
+ */
 /**
  http://www.meteolafleche.com/temperature.html
  Compute humidex for given relative humidity RH[%] and temperature T[Deg.C].
