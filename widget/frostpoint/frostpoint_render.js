da02f9afedb995a492cacc456d6ba4cbbd06bd22
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-06-11 21:46:13 +0200

    Add licence to huminex & frostpoint widgets
    Add licence to huminex & frostpoint widgets.

--- widget/frostpoint/frostpoint_render.js
+++ widget/frostpoint/frostpoint_render.js
@@ -1,3 +1,11 @@
+  /*
+   All Emoncms code is released under the GNU Affero General Public License.
+   See COPYRIGHT.txt and LICENSE.txt.
+    ---------------------------------------------------------------------
+    Emoncms - open source energy visualisation
+    Part of the OpenEnergyMonitor project:
+    http://openenergymonitor.org
+ */
 /**
  http://www.e-lab.de/downloads/DOCs/SHT11appnote2.pdf
  Compute frostPoint for given relative humidity RH[%] and temperature T[Deg.C].
