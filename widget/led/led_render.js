60e0dee43fb338a83b7814eb023b75b9a5bf2c76
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-06-26 16:58:17 +0200

    Update led_render.js (#102)
    code optimization

--- widget/led/led_render.js
+++ widget/led/led_render.js
@@ -27,31 +27,6 @@ function led_widgetlist()
   return widgets;
 }
 
-function led_init()
-{
-  setup_widget_canvas('led');
-}
-
-function led_draw() {
-  $('.led').each(function(index)
-  {
-    var feedid = $(this).attr("feedid");
-    if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
-    var val = associd[feedid]['value'] * 1;
-    var id = "can-"+$(this).attr("id");
-    if (browserVersion < 9)
-      draw_led_ie8(widgetcanvas[id], val);
-    else
-      draw_led(widgetcanvas[id], val);
-  });
-}
-
-function led_slowupdate() {
-  led_draw();
-}
-
-function led_fastupdate() {}
-
 function draw_led(circle,status){
   if (!circle) return;
 
@@ -68,26 +43,26 @@ function draw_led(circle,status){
   var radgrad = circle.createRadialGradient(width/2,height/2,0,width/2,height/2,dimension/2);
 
   if (status==0) {                   // red
-    radgrad.addColorStop(0, '#F75D59');
-    radgrad.addColorStop(0.9, '#C11B17');
+    radgrad.addColorStop(0, "#F75D59");
+    radgrad.addColorStop(0.9, "#C11B17");
   } else if (status>0 && status <=1) {            // green
-    radgrad.addColorStop(0, '#A7D30C');
-    radgrad.addColorStop(0.9, '#019F62');
+    radgrad.addColorStop(0, "#A7D30C");
+    radgrad.addColorStop(0.9, "#019F62");
   } else if (status>1 && status <=2) {           // grey
-    radgrad.addColorStop(0, '#736F6E');
-    radgrad.addColorStop(0.9, '#4A4344');
+    radgrad.addColorStop(0, "#736F6E");
+    radgrad.addColorStop(0.9, "#4A4344");
   } else if (status>2 && status <=3) { 		  //Blue
-    radgrad.addColorStop(0, '#00C9FF');
-    radgrad.addColorStop(0.9, '#00B5E2');
+    radgrad.addColorStop(0, "#00C9FF");
+    radgrad.addColorStop(0.9, "#00B5E2");
   } else if (status>3 && status <=4) {		  // Purple
-    radgrad.addColorStop(0, '#FF5F98');
-    radgrad.addColorStop(0.9, '#FF0188');
+    radgrad.addColorStop(0, "#FF5F98");
+    radgrad.addColorStop(0.9, "#FF0188");
   } else if (status>4 && status <=5)   {         // yellow
-    radgrad.addColorStop(0, '#F4F201');
-    radgrad.addColorStop(0.9, '#E4C700');
+    radgrad.addColorStop(0, "#F4F201");
+    radgrad.addColorStop(0.9, "#E4C700");
   } else {					  // Black
-    radgrad.addColorStop(0, '#000000');
-    radgrad.addColorStop(0.9, '#000000');
+    radgrad.addColorStop(0, "#000000");
+    radgrad.addColorStop(0.9, "#000000");
   }
 
   radgrad.addColorStop(1, 'rgba(1,159,98,0)');
@@ -122,6 +97,23 @@ function draw_led_ie8(circle,status){
   circle.fill()
 }
 
+function led_draw() {
+  $('.led').each(function(index)
+  {
+    var feedid = $(this).attr("feedid");
+    if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
+    var val = associd[feedid]["value"] * 1;
+    var id = "can-"+$(this).attr("id");
+    if (browserVersion < 9)
+      draw_led_ie8(widgetcanvas[id], val);
+    else
+      draw_led(widgetcanvas[id], val);
+  });
+}
+
+
+
+
 function draw_binary_led(circle,status){
   if (!circle) return;
   circle.clearRect(0,0,80,80);
@@ -129,11 +121,11 @@ function draw_binary_led(circle,status){
   var radgrad = circle.createRadialGradient(40,40,0,40,40,20);
 
   if (status==0) {                               // red
-    radgrad.addColorStop(0, '#F75D59');
-    radgrad.addColorStop(0.9, '#C11B17');
+    radgrad.addColorStop(0, "#F75D59");
+    radgrad.addColorStop(0.9, "#C11B17");
   } else {                                       // green
-    radgrad.addColorStop(0, '#A7D30C');
-    radgrad.addColorStop(0.9, '#019F62');
+    radgrad.addColorStop(0, "#A7D30C");
+    radgrad.addColorStop(0.9, "#019F62");
   }
 
   radgrad.addColorStop(1, 'rgba(1,159,98,0)');
@@ -157,4 +149,13 @@ function draw_binary_led_ie8(circle,status){
   circle.fill()
 }
 
+function led_init()
+{
+  setup_widget_canvas("led");
+}
 
+function led_slowupdate() {
+  led_draw();
+}
+
+function led_fastupdate() {}
