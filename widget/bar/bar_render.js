db39ecab6e6a7192ce7b8f28de3e6e1dae046923
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-07-04 23:10:12 +0200

    Update bar_render.js (#101)
    Add decimals, fontstyle, fontweight, position of unit options & optimization of the code

--- widget/bar/bar_render.js
+++ widget/bar/bar_render.js
@@ -40,20 +40,45 @@ function bar_widgetlist()
 
         }
     };
+    var decimalsDropBoxOptions = [        // Options for the type combobox. Each item is [typeID, "description"]
+                    [-1,   "Automatic"],
+                    [0,    "0"],
+                    [1,    "1"],
+                    [2,    "2"],
+                    [3,    "3"],
+                    [4,    "4"],
+                    [5,    "5"],
+                    [6,    "6"]
+    ];
 
     var fontoptions = [
                     [9, "Arial Black"],
-                    [8, "arial"],
-                    [7, "Arial Narrow"],
-                    [6, "sans-serif"],
+                    [8, "Arial Narrow"],
+                    [7, "sans-serif"],
+                    [6, "Helvetica Neue"],
                     [5, "Helvetica"],
                     [4, "Comic Sans MS"],
                     [3, "Courier New"],
-                    [2, "arial"],
+                    [2, "Arial"],
                     [1, "Georgia"],
                     [0, "Impact"]
                 ];
+
+    var fstyleoptions = [
+                    [2, "Normal"],
+                    [1, "Italic"],
+                    [0, "Oblique"]
+                ];
+
+    var fweightoptions = [
+                    [0, "Normal"],
+                    [1, "Bold"]
+                ];
     
+    var unitEndOptions = [
+                    [0, "Back"],
+                    [1, "Front"]
+                ];
     
     var graduationDropBoxOptions = [
                     [0, "Off"],
@@ -63,10 +88,14 @@ function bar_widgetlist()
     addOption(widgets["bar"], "title",          "value",            _Tr("Title"),           _Tr("Title of bar"),                                                                []);
     addOption(widgets["bar"], "colour_label",   "colour_picker",    _Tr("Label Colour"),    _Tr("Colour of title and values"),                                                  []);
     addOption(widgets["bar"], "font",           "dropbox",          _Tr("Font used"),       _Tr("Font used"),                                                                   fontoptions);
+    addOption(widgets["bar"], "fstyle",         "dropbox",          _Tr("Font style"),      _Tr("Font style used for display"),                                                 fstyleoptions);
+    addOption(widgets["bar"], "fweight",        "dropbox",          _Tr("Font weight"),     _Tr("Font weight used for display"),                                                fweightoptions);
     addOption(widgets["bar"], "feedid",         "feedid",           _Tr("Feed"),            _Tr("Feed value"),                                                                  []);
     addOption(widgets["bar"], "max",            "value",            _Tr("Max value"),       _Tr("Max value to show"),                                                           []);
     addOption(widgets["bar"], "scale",          "value",            _Tr("Scale"),           _Tr("Value is multiplied by scale before display. Defaults to 1"),                  []);
-    addOption(widgets["bar"], "units",          "value",            _Tr("Units"),           _Tr("Unit type to show after value. Ex: <br>\"{Reading}{unit-string}\""),           []);
+    addOption(widgets["bar"], "units",          "value",            _Tr("Units"),           _Tr("Units to show"),                                                               []);
+    addOption(widgets["bar"], "unitend",        "dropbox",          _Tr("Unit position"),   _Tr("Where should the unit be shown"),                                              unitEndOptions);
+    addOption(widgets["bar"], "decimals",       "dropbox",          _Tr("Decimals"),        _Tr("Decimals to show"),                                                            decimalsDropBoxOptions);
     addOption(widgets["bar"], "offset",         "value",            _Tr("Offset"),          _Tr("Static offset. Subtracted from value before computing position (default 0)"),  []);
     addOption(widgets["bar"], "colour",         "colour_picker",    _Tr("Colour"),          _Tr("Colour to draw bar in"),                                                       []);
     addOption(widgets["bar"], "graduations",    "dropbox",          _Tr("Graduations"),     _Tr("Should the graduations be shown"),                                             graduationDropBoxOptions);
@@ -76,63 +105,21 @@ function bar_widgetlist()
     return widgets;
 }
 
-function bar_init()
-{
-    setup_widget_canvas('bar');
-}
-
-function bar_draw()
-{
-    $('.bar').each(function(index)
-    {
-        var feedid = $(this).attr("feedid");
-        if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
-        var val = curve_value(feedid,dialrate).toFixed(3);
-        // ONLY UPDATE ON CHANGE
-        if (val != (associd[feedid]['value'] * 1).toFixed(3) || redraw == 1)
-        {
-            var id = "can-"+$(this).attr("id");
-            var scale = 1*$(this).attr("scale") || 1;
-            draw_bar(widgetcanvas[id],
-                                0,
-                                0,
-                                $(this).attr("title"),
-                                $(this).attr("font"),
-                                $(this).width(),
-                                $(this).height(),val*scale,
-                                $(this).attr("max"),
-                                $(this).attr("units"),
-                                $(this).attr("colour"),
-                                $(this).attr("colour_label"),
-                                $(this).attr("offset"),
-                                $(this).attr("graduations"),
-                                $(this).attr("gradNumber")
-                                );
-        }
-    });
-}
-
-function bar_slowupdate()
-{
-
-}
-
-function bar_fastupdate()
-{
-    bar_draw();
-}
-
 
 function draw_bar(context,
                 x_pos,              // these x and y coords seem unused?
                 y_pos,
                 title,
                 font,
+                fstyle,
+                fweight,
                 width,
                 height,
                 raw_value,
                 max_value,
                 units_string,
+                decimals,
+                unitend,
                 display_colour,
                 colour_label,
                 static_offset,
@@ -149,6 +136,9 @@ function draw_bar(context,
     // if units_string == false: "". Else units_string
     units_string = units_string || "";
     title = title || "";
+    fstyle = fstyle || "2";
+    fweight = fweight || "0";
+    unitend = unitend || "0";
     colour_label = colour_label || "000";
     static_offset = 1*static_offset || 0;
     var display_value = raw_value
@@ -163,20 +153,34 @@ function draw_bar(context,
         size = width/2;
     else
         size = height/2;
-    
-    if (font == 0){fontname = "Impact"}
-    if (font == 1){fontname = "Georgia"}
-    if (font == 2){fontname = "arial"}
-    if (font == 3){fontname = "Courier New"}
-    if (font == 4){fontname = "Comic Sans MS"}
-    if (font == 5){fontname = "Helvetica"}
-    if (font == 6){fontname = "sans-serif"}
-    if (font == 7){fontname = "Arial Narrow"}
-    if (font == 8){fontname = "Arial"}
-    if (font == 9){fontname = "Arial Black"}
-    else if (typeof(font) == "undefined") {fontname = "Arial Black"}
-    
-    if (graduationBool == 1)
+    size = size - (size*0.058/2);
+
+    var fontname;
+
+    if (font === "0"){fontname = "Impact";}
+    if (font === "1"){fontname = "Georgia";}
+    if (font === "2"){fontname = "Arial";}
+    if (font === "3"){fontname = "Courier New";}
+    if (font === "4"){fontname = "Comic Sans MS";}
+    if (font === "5"){fontname = "Helvetica";}
+    if (font === "6"){fontname = "Helvetica Neue";}
+    if (font === "7"){fontname = "sans-serif";}
+    if (font === "8"){fontname = "Arial Narrow";}
+    if (font === "9"){fontname = "Arial Black";}
+    else if (typeof(font) === "undefined") {fontname = "Arial Black"}
+
+    var fontstyle;
+
+    if (fstyle === "0"){fontstyle = "oblique";}
+    if (fstyle === "1"){fontstyle = "italic";}
+    if (fstyle === "2"){fontstyle = "normal";}
+
+    var fontweight;
+
+    if (fweight === "0"){fontweight = "normal";}
+    if (fweight === "1"){fontweight = "bold";}
+
+    if (graduationBool === "1")
     {
         height = height - (size/2)
         width = width - (size)
@@ -197,7 +201,7 @@ function draw_bar(context,
                        height-(border_space*2));
     context.lineWidth = 0;
     
-    if (display_colour.indexOf("#") == -1) display_colour = "#" + display_colour;  // Fix missing "#" on colour if needed
+    if (display_colour.indexOf("#") === -1) display_colour = "#" + display_colour;  // Fix missing "#" on colour if needed
 
     context.fillStyle = display_colour;
 
@@ -213,7 +217,7 @@ function draw_bar(context,
                     (height-bar_border_space) - bar_top );
 
                     
-    if (colour_label.indexOf("#") == -1) colour_label = "#" + colour_label; // Fix missing "#" on colour if needed
+    if (colour_label.indexOf("#") === -1) colour_label = "#" + colour_label; // Fix missing "#" on colour if needed
     context.fillStyle = colour_label;
     
     if (graduationBool == 1)
@@ -221,12 +225,13 @@ function draw_bar(context,
         if (graduationQuant > 0)
         {
             context.textAlign    = "start";
-            context.font = ((size*0.20)+"px "+ fontname);
+            context.font = (fontstyle+ " "+ fontweight+ " "+(size*0.15)+"px "+ fontname);
 
             var step = (height-border_space*2)/(Number(graduationQuant)+1);
             var curY;
             
-            context.fillText((static_offset+max_value)+units_string, width+(size*0.1), (size*0.15)+2);
+            if (unitend ==="0"){context.fillText((static_offset+max_value)+units_string, width+(size*0.1), (size*0.15)+2);}
+            if (unitend ==="1"){context.fillText(units_string+(static_offset+max_value), width+(size*0.1), (size*0.15)+2);}
             var divisions = Number(graduationQuant)+1;
             
             for (var y = 0; y < graduationQuant; y++)
@@ -240,44 +245,63 @@ function draw_bar(context,
                     unitOffset = unitOffset.toFixed(1)
                 else
                     unitOffset = unitOffset.toFixed(0)
-                context.fillText(parseFloat(unitOffset)+units_string, width+(size*0.1), curY+(size*0.05));
+                if (unitend ==="0"){context.fillText(parseFloat(unitOffset)+units_string, width+(size*0.1), curY+(size*0.05));}
+                if (unitend ==="1"){context.fillText(units_string+parseFloat(unitOffset), width+(size*0.1), curY+(size*0.05));}
             }
-            context.fillText(static_offset+units_string, width+(size*0.1), height-2);
+            if (unitend ==="0"){context.fillText(static_offset+units_string, width+(size*0.1), height-2);}
+            if (unitend ==="1"){context.fillText(units_string+static_offset, width+(size*0.1), height-2);}
 
             context.strokeStyle = "#888";
             context.stroke();
         }
     }
 
-    if (raw_value>=100) {
-        raw_value = raw_value.toFixed(0);
-    } else if (raw_value>=10) {
-        raw_value = raw_value.toFixed(1);
-    } else  {
-        raw_value = raw_value.toFixed(2);
-    }
-    raw_value = parseFloat(raw_value);
+        if (decimals<0){
+            if (raw_value>=100) {
+            raw_value = raw_value.toFixed(0);
+            } else if (raw_value>=10) {
+            raw_value = raw_value.toFixed(1);
+            } else  {
+            raw_value = raw_value.toFixed(2);
+            }
+        raw_value = parseFloat(raw_value);
+        }
+        else {
+             raw_value = raw_value.toFixed(decimals);
+        }
+
 
     context.fillStyle = colour_label;
-    context.textAlign    = "center";
+    
+    var unitsandval = raw_value+units_string;
+    var valsize;
+    if (unitsandval.length >4){ valsize = (size / (unitsandval.length+2)) * 5.5;}
+	else {valsize = (size / 6) * 5.5;}
+    var titlesize ;
+	if (title.length >10) {titlesize = (size / (title.length+2)) * 9;}
+	else {titlesize = (size / 12) * 9.5;}
+    
     if (graduationBool == 1) {
-        half_width += (size*0.20)
-
+    context.textAlign    = "start";
         if (title) {
-            context.font = ((size*0.25)+"px "+ fontname);
-            context.fillText(raw_value+units_string, half_width+(size*0.40), height + (size*0.48));
-            context.font = ((size*0.20)+"px "+ fontname);
-            context.fillText(title, half_width + (size * 0.35), height + (size * 0.2));
+            context.font = (fontstyle+ " "+ fontweight+ " "+(valsize*0.3)+"px "+ fontname);
+            if (unitend ==="0"){context.fillText(raw_value+units_string, bar_border_space, height + (size*0.42))}
+            if (unitend ==="1"){context.fillText(units_string+raw_value, bar_border_space, height + (size*0.42))}
+            context.font = (fontstyle+ " "+ fontweight+ " "+(titlesize*0.35)+"px "+ fontname);
+            context.fillText(title, bar_border_space, height + (size * 0.2));
         } else {
-            context.font = ((size*0.3)+"px "+ fontname);
-            context.fillText(raw_value+units_string, half_width+(size*0.2), height + (size*0.3));
+            context.font = (fontstyle+ " "+ fontweight+ " "+(valsize*0.45)+"px "+ fontname);
+            if (unitend ==="0"){context.fillText(raw_value+units_string, bar_border_space, height + (size*0.3));}
+            if (unitend ==="1"){context.fillText(units_string+raw_value, bar_border_space, height + (size*0.3));}
         }
     }
     else
     {
-        context.font = ((size*0.5)+"px "+ fontname);
-        context.fillText(raw_value+units_string, half_width, height/2 + (size*0.2));
-        context.font = ((size*0.2)+"px "+ fontname);
+    context.textAlign    = "center";
+        context.font = (fontstyle+ " "+ fontweight+ " "+(valsize*0.5)+"px "+ fontname);
+        if (unitend ==="0"){context.fillText(raw_value+units_string, half_width, height/2 + (size*0.2));}
+        if (unitend ==="1"){context.fillText(units_string+raw_value, half_width, height/2 + (size*0.2));}
+        context.font = (fontstyle+ " "+ fontweight+ " "+(titlesize*0.4)+"px "+ fontname);
         context.fillText(title, half_width, height/7 + (size *0.1));
     }
 
@@ -285,4 +309,53 @@ function draw_bar(context,
     var spreadAngle = 32;
 
 }
+function bar_draw()
+{
+    $('.bar').each(function(index)
+    {
+        var feedid = $(this).attr("feedid");
+        if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
+        var val = curve_value(feedid,dialrate).toFixed(3);
+        // ONLY UPDATE ON CHANGE
+        if (val != (associd[feedid]['value'] * 1).toFixed(3) || redraw == 1)
+        {
+            var id = "can-"+$(this).attr("id");
+            var scale = 1*$(this).attr("scale") || 1;
+            draw_bar(widgetcanvas[id],
+                                0,
+                                0,
+                                $(this).attr("title"),
+                                $(this).attr("font"),
+                                $(this).attr("fstyle"),
+                                $(this).attr("fweight"),
+                                $(this).width(),
+                                $(this).height(),
+                                val*scale,
+                                $(this).attr("max"),
+                                $(this).attr("units"),
+                                $(this).attr("decimals"),
+                                $(this).attr("unitend"),
+                                $(this).attr("colour"),
+                                $(this).attr("colour_label"),
+                                $(this).attr("offset"),
+                                $(this).attr("graduations"),
+                                $(this).attr("gradNumber")
+                                );
+        }
+    });
+}
 
+
+function bar_init()
+{
+    setup_widget_canvas('bar');
+}
+function bar_slowupdate()
+{
+
+}
+
+function bar_fastupdate()
+{
+    bar_draw();
+}
