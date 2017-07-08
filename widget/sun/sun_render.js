b91ec3220ef22219119447d66d1722eadf315b35
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-07-08 12:42:36 +0200

    add options
    add decimals, fontstyle, fontweight, position of unit options
    optimization of the code
    should not break existing widget - need to test

--- widget/sun/sun_render.js
+++ widget/sun/sun_render.js
@@ -37,6 +37,16 @@ function sun_widgetlist(){
     }
   };
 
+  var decimalsDropBoxOptions = [        // Options for the type combobox. Each item is [typeID, "description"]
+    [-1,   "Automatic"],
+    [0,    "0"],
+    [1,    "1"],
+    [2,    "2"],
+    [3,    "3"],
+    [4,    "4"],
+    [5,    "5"],
+    [6,    "6"]
+    ];
   var fontoptions = [
     [8, "Arial Black"],
     [7, "Arial Narrow"],
@@ -48,15 +58,34 @@ function sun_widgetlist(){
     [1, "Georgia"],
     [0, "Impact"]
   ];
+  var fstyleoptions = [
+    [2, "Normal"],
+    [1, "Italic"],
+    [0, "Oblique"]
+  ];
 
-  addOption(widgets["sun"], "feedid",      "feedid",  _Tr("Feed"),        _Tr("Feed value"),                                                            []);
-  addOption(widgets["sun"], "max",         "value",   _Tr("Max value"),   _Tr("Max value to show"),                                                     []);
-  addOption(widgets["sun"], "scale",       "value",   _Tr("Scale"),       _Tr("Value is multiplied by scale before display"),                           []);
-  addOption(widgets["sun"], "units",       "value",   _Tr("Units"),       _Tr("Units to show"),                                                         []);
-  addOption(widgets["sun"], "offset",      "value",   _Tr("Offset"),      _Tr("Static offset. Subtracted from value before computing needle position"), []);
-  addOption(widgets["sun"], "solar_title",       "value",   _Tr("solar title"),       _Tr("Solar title"),                                     []);
-  addOption(widgets["sun"], "colour",      "colour_picker",   _Tr("Colour label"),      _Tr("Color of the label"),                                  []);
-  addOption(widgets["sun"], "font",      "dropbox",   _Tr("Font"),      _Tr("Label font"),                                                  fontoptions);
+  var fweightoptions = [
+    [0, "Normal"],
+    [1, "Bold"]
+  ];
+
+  var unitEndOptions = [
+    [0, "Back"],
+    [1, "Front"]
+  ];
+
+  addOption(widgets["sun"],   "feedid",       "feedid",          _Tr("Feed"),          _Tr("Feed value"),                                            []);
+  addOption(widgets["sun"],   "max",          "value",           _Tr("Max value"),     _Tr("Max value to show"),                                     []);
+  addOption(widgets["sun"],   "scale",        "value",           _Tr("Scale"),         _Tr("Value is multiplied by scale before display"),           []);
+  addOption(widgets["sun"],   "units",        "value",           _Tr("Units"),         _Tr("Units to show"),                                         []);
+  addOption(widgets["sun"],   "unitend",      "dropbox",         _Tr("Unit position"), _Tr("Where should the unit be shown"),                        unitEndOptions);
+  addOption(widgets["sun"],   "decimals",     "dropbox",         _Tr("Decimals"),      _Tr("Decimals to show"),                                      decimalsDropBoxOptions);
+  addOption(widgets["sun"],   "offset",       "value",           _Tr("Offset"),        _Tr("Static offset. Subtracted from value before computing"), []);
+  addOption(widgets["sun"],   "solar_title",  "value",           _Tr("solar title"),   _Tr("Solar title"),                                           []);
+  addOption(widgets["sun"],   "colour",       "colour_picker",   _Tr("Colour label"),  _Tr("Color of the label"),                                    []);
+  addOption(widgets["sun"],   "font",         "dropbox",         _Tr("Font"),          _Tr("Label font"),                                            fontoptions);
+  addOption(widgets["sun"],   "fstyle",       "dropbox",         _Tr("Font style"),    _Tr("Font style used for display"),                           fstyleoptions);
+  addOption(widgets["sun"],   "fweight",      "dropbox",         _Tr("Font weight"),   _Tr("Font weight used for display"),                          fweightoptions);
 
   return widgets;
 }
@@ -78,22 +107,38 @@ function sun_draw(){
       var offset = 1*$(this).attr("offset") || 0;
       var max_val = 1*$(this).attr("max") || 100;
       var units = $(this).attr("units");
+      var decimals = $(this).attr("decimals");
+      var unitend = $(this).attr("unitend") || "0";
       var font = $(this).attr("font");
+      var fstyle = $(this).attr("fstyle") || "2";
+      var fweight = $(this).attr("fweight") || "0";
       var color = $(this).attr("colour") || "000";
       var title = $(this).attr("solar_title");
 
-      if (font == 0){fontname = "Impact"}
-      if (font == 1){fontname = "Georgia"}
-      if (font == 2){fontname = "Arial"}
-      if (font == 3){fontname = "Courier New"}
-      if (font == 4){fontname = "Comic Sans MS"}
-      if (font == 5){fontname = "Helvetica"}
-      if (font == 6){fontname = "sans-serif"}
-      if (font == 7){fontname = "Arial Narrow"}
-      if (font == 8){fontname = "Arial Black"}
-      else if (typeof(font) == "undefined") {fontname = "Arial Black"}
+      var fontname;
+      if (font === "0"){fontname = "Impact";}
+      if (font === "1"){fontname = "Georgia";}
+      if (font === "2"){fontname = "Arial";}
+      if (font === "3"){fontname = "Courier New";}
+      if (font === "4"){fontname = "Comic Sans MS";}
+      if (font === "5"){fontname = "Helvetica";}
+      if (font === "6"){fontname = "sans-serif";}
+      if (font === "7"){fontname = "Arial Narrow";}
+      if (font === "8"){fontname = "Arial Black";}
+      else if (typeof(font) === "undefined") {fontname = "Arial Black";}
 
-      if (color.indexOf("#") == -1) color = "#" + color;
+      var fontstyle;
+
+      if (fstyle === "0"){fontstyle = "oblique";}
+      if (fstyle === "1"){fontstyle = "italic";}
+      if (fstyle === "2"){fontstyle = "normal";}
+
+      var fontweight;
+
+      if (fweight === "0"){fontweight = "normal";}
+      if (fweight === "1"){fontweight = "bold";}
+
+      if (color.indexOf("#") === -1) color = "#" + color;
 
       var start_x = 0, start_y = 0;
       var sun_height = $(this).height();
@@ -107,21 +152,29 @@ function sun_draw(){
       if(data > max_val)
         data = max_val;
 
-      if (data>=100) {
-          data = data.toFixed(0);
-      } else if (data>=10) {
-          data = data.toFixed(1);
-      } else  {
-          data = data.toFixed(2);
+      if (decimals<0){
+        if (data>=100) {
+            data = data.toFixed(0);
+        } else if (data>=10) {
+            data = data.toFixed(1);
+        } else  {
+            data = data.toFixed(2);
+        }
+        data = parseFloat(data);
       }
-      data = parseFloat(data);
+      else {
+           data = data.toFixed(decimals);
+      }
 
       var context = widgetcanvas[id];
 
       context.globalAlpha = 1;
 
       var bar_length = 10;
-      var radius = sun_height>sun_width?(sun_height - bar_length)/3:(sun_width - bar_length)/3;
+      var bar_width = 5;
+      var number_of_bars = 5;
+	  
+      var radius = Math.max(Math.min(sun_width/2,sun_height) - bar_length - bar_width,0);
   
       var centerX = sun_width / 2;
       var centerY = sun_height;
@@ -141,8 +194,6 @@ function sun_draw(){
       context.stroke();
 
       //Draw the bars
-      var bar_width = 5;
-      var number_of_bars = 5;
 
       context.lineWidth = line_width;
       context.strokeStyle = '#FFE87C';//'#FDB813';
@@ -162,18 +213,27 @@ function sun_draw(){
       }
       context.stroke();
 
-      var size = radius/2;
-      context.font      = fontname;
+      var size = radius;
+
+      var unitsandval = data +units;
+      var valsize;
+      if (unitsandval.length >4){ valsize = (size / (unitsandval.length+2)) * 5.5;}
+      else {valsize = (size / 6) * 5.5;}
+      var titlesize ;
+      if (title.length >10) {titlesize = (size / (title.length+2)) * 9;}
+      else {titlesize = (size / 12) * 9.5;}
+
       context.fillStyle = color;
       context.textAlign = "center";
-      context.font = ((size*0.50)+"px "+ fontname);
-      context.fillText(data + units, start_x + sun_width/2, start_y + centerY - radius*0.2);
-
+      context.font = (fontstyle+ " "+ fontweight+ " "+(valsize*0.50)+"px "+ fontname);
+      if (unitend ==="0"){context.fillText(data + units, start_x + sun_width/2, start_y + centerY - radius*0.2);}
+      if (unitend ==="1"){context.fillText(units + data, start_x + sun_width/2, start_y + centerY - radius*0.2);}
+	  
       if(title)
       {
         context.fillStyle = color;
         context.textAlign = "center";
-        context.font = ((size*0.20)+"px "+ fontname);
+        context.font = (fontstyle+ " "+ fontweight+ " "+(titlesize*0.25)+"px "+ fontname);
         context.fillText(title, start_x + sun_width/2, start_y + centerY - radius*0.6);
       }
     }
