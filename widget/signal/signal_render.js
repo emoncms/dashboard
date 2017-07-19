e8b71ce280d79dd5bf1c61788e3a2729968eea10
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-07-19 23:04:48 +0200

    Add font options - other improvements
    Add fontweight and fontstyle options
    Auto resizing of the title
    Prevents from a truncation of the graph

--- widget/signal/signal_render.js
+++ widget/signal/signal_render.js
@@ -49,67 +49,93 @@ function signal_widgetlist(){
     [0, "Impact"]
   ];
 
-  addOption(widgets["signal"], "feedid",      "feedid",  _Tr("Feed"),        _Tr("Feed value"),                                                            []);
-  addOption(widgets["signal"], "max",         "value",   _Tr("Max value"),   _Tr("Max value to show"),                                                     []);
-  addOption(widgets["signal"], "scale",       "value",   _Tr("Scale"),       _Tr("Value is multiplied by scale before display"),                           []);
-  // addOption(widgets["signal"], "units",       "value",   _Tr("Units"),       _Tr("Units to show"),                                                      []);
-  addOption(widgets["signal"], "offset",      "value",   _Tr("Offset"),      _Tr("Static offset. Subtracted from value before computing"), []);
-  addOption(widgets["signal"], "signal_title",       "value",   _Tr("Signal title"),       _Tr("Signal title"),                                             []);
-  addOption(widgets["signal"], "colour1",      "colour_picker",   _Tr("Colour signal"),      _Tr("Color of the signal"),                                      []);
-  addOption(widgets["signal"], "colour",      "colour_picker",   _Tr("Colour label"),      _Tr("Color of the label"),                 []);
-  addOption(widgets["signal"], "font",      "dropbox",   _Tr("Font"),      _Tr("Label font"),                                                  fontoptions);
+  var fstyleoptions = [
+    [2, "Normal"],
+    [1, "Italic"],
+    [0, "Oblique"]
+  ];
+
+  var fweightoptions = [
+    [0, "Normal"],
+    [1, "Bold"]
+
+  ];
+
+  addOption(widgets["signal"], "feedid",         "feedid",         _Tr("Feed"),           _Tr("Feed value"),                                            []);
+  addOption(widgets["signal"], "max",            "value",          _Tr("Max value"),      _Tr("Max value to show"),                                     []);
+  addOption(widgets["signal"], "scale",          "value",          _Tr("Scale"),          _Tr("Value is multiplied by scale before display"),           []);
+  addOption(widgets["signal"], "offset",         "value",          _Tr("Offset"),         _Tr("Static offset. Subtracted from value before computing"), []);
+  addOption(widgets["signal"], "signal_title",   "value",          _Tr("Signal title"),   _Tr("Signal title"),                                          []);
+  addOption(widgets["signal"], "colour1",        "colour_picker",  _Tr("Colour signal"),  _Tr("Color of the signal"),                                   []);
+  addOption(widgets["signal"], "colour",         "colour_picker",  _Tr("Colour label"),   _Tr("Color of the label"),                                    []);
+  addOption(widgets["signal"], "font",           "dropbox",        _Tr("Font"),           _Tr("Label font"),                                            fontoptions);
+  addOption(widgets["signal"], "fstyle",         "dropbox",        _Tr("Font style"),     _Tr("Font style used for display"),                           fstyleoptions);
+  addOption(widgets["signal"], "fweight",        "dropbox",        _Tr("Font weight"),    _Tr("Font weight used for display"),                          fweightoptions);
 
   return widgets;
 }
 
 function signal_init(){
-  setup_widget_canvas('signal');
+  setup_widget_canvas("signal");
 }
 
 function signal_draw(){
-  $('.signal').each(function(index) {
+  $(".signal").each(function(index) {
     var feedid = $(this).attr("feedid");
     if (associd[feedid] === undefined) { console.log("Review config for feed id of " + $(this).attr("class")); return; }
     var val = curve_value(feedid,dialrate).toFixed(3);
     // ONLY UPDATE ON CHANGE
-    if (val != (associd[feedid]['value'] * 1).toFixed(3) || redraw == 1)
+    if (val != (associd[feedid]["value"] * 1).toFixed(3) || redraw == 1)
     {
       var id = "can-"+$(this).attr("id");
       var scale = 1*$(this).attr("scale") || 1;
       var offset = 1*$(this).attr("offset") || 0;
       var max_val = 1*$(this).attr("max") || 100;
-      // var units = $(this).attr("units");
-      var font = $(this).attr("font");
+      var font = $(this).attr("font") || "8";
+      var fstyle = $(this).attr("fstyle") || "2";
+      var fweight = $(this).attr("fweight") || "0";
       var color = $(this).attr("colour") || "000";
       var title = $(this).attr("signal_title");
       var colour1 = $(this).attr("colour1") || "000";
 
-      if (font == 0){fontname = "Impact"}
-      if (font == 1){fontname = "Georgia"}
-      if (font == 2){fontname = "Arial"}
-      if (font == 3){fontname = "Courier New"}
-      if (font == 4){fontname = "Comic Sans MS"}
-      if (font == 5){fontname = "Helvetica"}
-      if (font == 6){fontname = "sans-serif"}
-      if (font == 7){fontname = "Arial Narrow"}
-      if (font == 8){fontname = "Arial Black"}
-      else if (typeof(font) == "undefined") {fontname = "Arial Black"}
+      var fontname;
+
+      if (font === "0"){fontname = "Impact"};
+      if (font === "1"){fontname = "Georgia"};
+      if (font === "2"){fontname = "Arial"};
+      if (font === "3"){fontname = "Courier New"};
+      if (font === "4"){fontname = "Comic Sans MS"};
+      if (font === "5"){fontname = "Helvetica"};
+      if (font === "6"){fontname = "sans-serif"};
+      if (font === "7"){fontname = "Arial Narrow"};
+      if (font === "8"){fontname = "Arial Black"};
+
+      var fontstyle;
+
+      if (fstyle === "0"){fontstyle = "oblique";}
+      if (fstyle === "1"){fontstyle = "italic";}
+      if (fstyle === "2"){fontstyle = "normal";}
+
+      var fontweight;
+
+      if (fweight === "0"){fontweight = "normal";}
+      if (fweight === "1"){fontweight = "bold";}
 
       console.log(color, colour1);
-      if (color.indexOf("#") == -1) color = "#" + color;
-      if (colour1.indexOf("#") == -1) colour1 = "#" + colour1;
+      if (color.indexOf("#") === -1) {color = "#" + color;}
+      if (colour1.indexOf("#") === -1) {colour1 = "#" + colour1;}
 
       var start_x = 0, start_y = 0;
       var signal_height = $(this).height();
       var signal_width = $(this).width();
-      var line_width = signal_width/20;
+      var line_width = Math.min(signal_width,signal_height)/20;
       var margin = 1;
       var number_of_blocks = 5;
 
       var data = val*scale + offset;
       
-      if(data > max_val)
-        data = max_val;
+      if(data > max_val){
+	  data = max_val;}
 
       if (data>=100) {
           data = data.toFixed(0);
@@ -122,10 +148,12 @@ function signal_draw(){
 
       var context = widgetcanvas[id];
 
+      context.clearRect(0,0,signal_width,signal_height); // Clear old drawing
+
       context.globalAlpha = 1;
 
       var bar_length = 10;
-      var radius = signal_width/(0.5*Math.PI);
+      var radius = Math.min(signal_width,signal_height)/(0.5*Math.PI);
       var block_width = radius/number_of_blocks - line_width;
   
       var centerX = signal_width/2;
@@ -161,17 +189,15 @@ function signal_draw(){
       }
 
       var size = radius/2;
-      // context.font      = fontname;
-      // context.fillStyle = color;
-      // context.textAlign = "center";
-      // context.font = ((size*0.50)+"px "+ fontname);
-      // context.fillText(data + units, start_x + signal_width/2, start_y + centerY - radius*0.2);
 
       if(title)
       {
+        var titlesize ;
+        if (title.length >10) {titlesize = (size / (title.length+2)) * 9;}
+        else {titlesize = (size / 12) * 9.5;}
         context.fillStyle = color;
         context.textAlign = "center";
-        context.font = ((size*0.20)+"px "+ fontname);
+        context.font = (fontstyle+ " "+ fontweight+ " "+ (titlesize*0.70)+"px "+ fontname);
         context.fillText(title, start_x + signal_width/2, start_y + centerY - radius*0.6);
       }
     }
