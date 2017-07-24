098d78eb47a0ce8057751cc4a16674b9be0cc68a
Aymeric THIBAUT <thib66@users.noreply.github.com>
2017-07-24 22:55:14 +0200

    add localization of drop-down lists and license update

--- widget/heatindex/heatindex_render.js
+++ widget/heatindex/heatindex_render.js
@@ -1,7 +1,7 @@
- /*
-   All emon_widgets code is released under the GNU General Public License v3.
-   See COPYRIGHT.txt and LICENSE.txt.
-
+/*
+  All Emoncms code is released under the GNU Affero General Public License.
+  See COPYRIGHT.txt and LICENSE.txt.
+    ---------------------------------------------------------------------
    Part of the OpenEnergyMonitor project: http://openenergymonitor.org
    Authors : Paul Reed & Aymeric Thibaut
  */
@@ -66,7 +66,7 @@ function heatindex_widgetlist()
     ];
     
     var decimalsDropBoxOptions = [        // Options for the type combobox. Each item is [typeID, "description"]
-        [-1,   "Automatic"],
+        [-1,   _Tr("Automatic")],
         [0,    "0"],
         [1,    "1"],
         [2,    "2"],
@@ -90,14 +90,14 @@ function heatindex_widgetlist()
 				];
 	
 	var fstyleoptions = [
-					[2, "Normal"],
-					[1, "Italic"],
-					[0, "Oblique"]
+					[2, _Tr("Normal")],
+					[1, _Tr("Italic")],
+					[0, _Tr("Oblique")]
 				];
 				
 	var fweightoptions = [
-					[1, "Bold"],
-					[0, "Normal"]
+					[1, _Tr("Bold")],
+					[0, _Tr("Normal")]
 				];
 				
 	var sizeoptions = [
@@ -118,18 +118,18 @@ function heatindex_widgetlist()
 					[0, "6"]
 				];
 	var unitEndOptions = [
-					[0, "Back"],
-					[1, "Front"]
+					[0, _Tr("Back")],
+					[1, _Tr("Front")]
 				];
 				
 	var rule1options = [
 					[0, "Heat Index"],
-					[1, "Temperature Feed"]
+					[1, _Tr("Temperature Feed")]
 				];
 				
 	var rule2options = [
-					[0, "Temperature Feed"],
-					[1, "Display Nothing"]
+					[0, _Tr("Temperature Feed")],
+					[1, _Tr("Display Nothing")]
 				];	
     
     addOption(widgets["heatindex"], "feedhumid", "feedid",        _Tr("Humidity"),      _Tr("Relative humidity in %"),                      []);
