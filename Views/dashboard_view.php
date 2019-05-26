3de8e0d2ad00ab88e8db8136c4d38556e7e5bdca
Emrys <emrys@openenergymonitor.org>
2019-05-26 12:09:18 +0100

    removed link to unrequired js file

--- Views/dashboard_view.php
+++ Views/dashboard_view.php
@@ -34,7 +34,6 @@ if ($session['write']) $dashboard_editor_icon ='<a href="'.$path.'dashboard/edit
   <script type="text/javascript" src="<?php echo $path; ?>Modules/dashboard/Views/js/widgetlist.js?ver=<?php echo $js_css_version; ?>"></script>
   <script type="text/javascript" src="<?php echo $path; ?>Modules/dashboard/Views/js/render.js?ver=<?php echo $js_css_version; ?>"></script>
   <script type="text/javascript" src="<?php echo $path; ?>Modules/feed/feed.js?ver=<?php echo $js_css_version; ?>"></script>
-  <script type="text/javascript" src="<?php echo $path; ?>Modules/dashboard/dashboard.js?ver=<?php echo $js_css_version; ?>"></script>
   <?php require_once "Modules/dashboard/Views/loadwidgets.php"; ?>
 <h2 class="d-none"><?php echo $dashboard['name'] ?></h2>
  <div id="editicon" class="hidden-phone">
